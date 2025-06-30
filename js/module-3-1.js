9ead3294bd46448103c489745445818b4e16b580
Serhii_Paliienko <sergiopalienko@gmail.com>
2025-06-30 23:25:28 +0200

    module-3-1

---
+++ js/module-3-1.js
@@ -0,0 +1,97 @@
+// const cources = ['HTML', 'CSS', 'JS', 'React', 'NodeJs'];
+// console.log(cources);
+// console.log(cources[2]);
+
+// cources[0] = 'React';
+// console.log(cources);
+
+// console.log(cources.length);
+
+// const firstElement = cources[0];
+// console.log('firstElement:', firstElement);
+
+// const lastElement = cources[cources.length - 1];
+// console.log('lastElement:', lastElement);
+
+// !
+// let a = 10;
+// const b = a;
+
+// console.log('a:', a);
+// console.log('b:', b);
+
+// a = 20;
+
+// console.log('a:', a);
+// console.log('b:', b);
+
+// !
+// const arr1 = [1, 2, 3];
+// const arr2 = arr1;
+
+// console.log('arr1:', arr1);
+// console.log('arr2:', arr2);
+
+// arr2[0] = 100;
+
+// console.log('arr1:', arr1);
+// console.log('arr2:', arr2);
+
+// console.log(arr1 === arr2);
+
+// const arr3 = [1, 2, 3];
+// const arr4 = [1, 2, 3];
+
+// console.log(arr3 === arr4);
+
+// !
+// const courses = ['HTML', 'CSS', 'JS', 'React', 'NodeJs'];
+// const coursesStr = courses.join('-=-');
+
+// console.log('courses:', courses);
+// console.log('coursesStr:', coursesStr);
+
+// !
+// const courses = ['HTML', 'CSS', 'JS', 'React', 'NodeJs'];
+// const coursesCopy = courses.slice();
+
+// console.log('courses:', courses);
+// console.log('coursesCopy:', coursesCopy);
+
+// coursesCopy[0] = 'PHP';
+
+// console.log('courses:', courses);
+// console.log('coursesCopy:', coursesCopy);
+
+// !
+// const oldCourses = ['HTML', 'CSS'];
+// const newCourses = ['JS', 'React'];
+// const courses = ['NodeJs'];
+
+// const allCourses = courses.concat(newCourses, newCourses);
+
+// console.log(allCourses);
+
+// !
+// const courses = ['HTML', 'CSS', 'JS', 'React', 'NodeJs'];
+// const indexOfJS = courses.indexOf('JS');
+// console.log(indexOfJS);
+
+// !
+// const courses = ['HTML', 'CSS', 'JS', 'React', 'NodeJs'];
+// console.log(courses);
+
+// courses[courses.length] = 'PHP';
+// console.log(courses);
+
+// courses.push('Java', 'SASS');
+// courses.unshift('C++');
+// console.log(courses);
+
+// courses.pop();
+// console.log(courses);
+
+// courses.shift();
+// console.log(courses);
+
+// !
--- js/task-1.js
+++ js/task-1.js
@@ -1,16 +1,180 @@
-function isEnoughCapacity(products, containerSize) {
-  const values = Object.values(products);
-  let total = 0;
-  for (const value of values) {
-    total += value;
-  }
-  return containerSize >= total;
-}
+// const fullName = 'Serhii Paliienko';
+// const fullNameCopy = fullName.slice(3, 8);
 
-console.log(isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)); // true
+// console.log('fullName:', fullName);
+// console.log('fullNameCopy:', fullNameCopy);
 
-console.log(isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)); // false
+// !
+// const userEmail = 'test@GMAIL.com';
 
-console.log(isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14)); // true
+// console.log('userEmail:', userEmail);
+// console.log('userEmailtoLowerCase:', userEmail.toLowerCase());
 
-console.log(isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7)); // false
+// !
+// const userEmail = 'test@gmail.com';
+// const isEmailValid = userEmail.includes('@');
+// const isEmailValid = userEmail.startsWith('test');
+// const isEmailValid = userEmail.endsWith('com');
+// const isEmailValid = userEmail.endsWith('us');
+
+// console.log('userEmail:', userEmail);
+// console.log(isEmailValid);
+
+// !
+
+// const userEmail = '  test@gmailtest.com  ';
+// const isEmailValid = userEmail.indexOf('@');
+// const isEmailValid = userEmail.lastIndexOf('test');
+// const isEmailValid = userEmail.trim();
+
+// console.log('userEmail:', userEmail);
+// console.log(isEmailValid);
+
+// !
+// let hours = '1';
+// let minutes = '7';
+// let seconds = '24';
+
+// console.log(`${hours}:${minutes}:${seconds}`);
+
+// hours = hours.padStart(2, 0);
+// minutes = minutes.padStart(2, 0);
+// seconds = seconds.padStart(2, 0);
+
+// console.log(`${hours}:${minutes}:${seconds}`);
+
+// hours = hours.padEnd(3, 0);
+// seconds = seconds.padEnd(3, 0);
+// minutes = minutes.padEnd(3, 0);
+
+// console.log(`${hours}:${minutes}:${seconds}`);
+
+// ???
+// !
+// const answer = 'ECMAScript';
+
+// const answer = 'ecmascript';
+
+// let userInput = prompt('Яка "офіційна" назва JavaScript?') || '';
+
+// userInput = userInput.toLowerCase().trim();
+
+// if (userInput.toLowerCase().trim() === answer) {
+//   alert('Вірно!');
+// } else {
+//   alert('Не знаєте? ECMAScript!');
+// }
+
+// switch (userInput) {
+//   case answer: {
+//     alert('Вірно!');
+
+//     break;
+//   }
+
+//   default: {
+//     alert('Не знаєте? ECMAScript!');
+//   }
+// }
+
+// !
+
+// let link = 'https://my-site.com/about';
+
+// if (!link.endsWith('/') && link.includes('my-site')) {
+//   link += '/';
+// }
+
+// if (!link.endsWith('/')) {
+//   link += '/';
+// }
+
+// console.log(link);
+
+// ?
+// !
+// let a = 10;
+// console.log('a:', a);
+
+// ++a;
+
+// console.log('a:', a);
+
+// !
+// for (let i = 2; i <= 10; i++) {
+//   if (i % 2 === 0) {
+//     console.log(i);
+//   }
+// }
+
+// !
+// let min = 5;
+// let max = 201;
+
+// for (let i = min; i <= max; i++) {
+//   if (i % 5 === 0) {
+//     console.log(i);
+//   }
+// }
+
+// !
+// const min = 1;
+// const max = 10;
+
+// let total = 0;
+// for (let i = min; i <= max; i++) {
+//   if (i % 2 === 0) {
+//     total += i;
+//   }
+// }
+
+// console.log(total);
+
+// !
+// for (let i = 1; i <= 100; i++) {
+//   if (i % 3 === 0 && i % 5 === 0) {
+//     console.log('FizzBuzz');
+//   } else if (i % 5 === 0) {
+//     console.log('Buzz');
+//   } else if (i % 3 === 0) {
+//     console.log('Fizz');
+//   } else {
+//     console.log(i);
+//   }
+// }
+
+// !
+// let firstEvenNumber = 0;
+
+// for (let i = 100; i <= 200; i++) {
+//   if (i % 7 === 0) {
+// console.log(i);
+//     firstEvenNumber = i;
+//     break;
+//   }
+// }
+// console.log(firstEvenNumber);
+
+// !
+
+// function isPalindrome(str) {
+//   console.log(str);
+//   let reversedStr = '';
+
+//   for (let i = str.length - 1; i >= 0; i--) {
+//     reversedStr += str[i];
+//   }
+//   return str.toLowerCase() === reversedStr.toLowerCase();
+// }
+
+// console.log(isPalindrome('level'));
+// console.log(isPalindrome('cat'));
+// console.log(isPalindrome('madam'));
+
+// const firstName = 'Serhii';
+
+// console.log(firstName);
+
+// for (let i = firstName.length - 1; i >= 0; i--) {
+//   console.log(firstName[i]);
+// }
